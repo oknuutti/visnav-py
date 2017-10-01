fix keypoint algo rotation update, other changes

src/algo/keypoint.py
@@ -20,6 +20,8 @@ class KeypointAlgo():
         
         self.debug_filebase = None
         
+        self.DEBUG_IMG_POSTFIX = 'k'   # fi batch mode, save result image in a file ending like this
+        
         self.MIN_FEATURES = 8          # fail if less inliers at the end
         self.MODEL_DISTANCE_COEF = 1.6 # default 1.6
         self.LOWE_METHOD_COEF = 0.75   # default 0.7
@@ -30,6 +32,7 @@ class KeypointAlgo():
     def solve_pnp(self, sce_img, **kwargs):
         # maybe load scene image
         if isinstance(sce_img, str):
+            self.debug_filebase = sce_img[0:-4]+self.DEBUG_IMG_POSTFIX
             self.glWidget.loadTargetImage(sce_img, remove_bg=False)
             sce_img = self.glWidget.full_image
 
@@ -52,7 +55,7 @@ class KeypointAlgo():
         # debug by drawing matches
         if not BATCH_MODE or DEBUG:
             print('matches: %s/%s'%(len(matches), min(len(sce_kp), len(ref_kp))), flush=True, end=", ")
-            self._draw_matches(sce_img, sce_kp, ref_img, ref_kp, matches, pause=False)
+        self._draw_matches(sce_img, sce_kp, ref_img, ref_kp, matches, pause=False, show=DEBUG)
         
         if error is not None:
             raise error
@@ -61,8 +64,9 @@ class KeypointAlgo():
         ref_kp_3d = self._inverse_project([ref_kp[m.trainIdx].pt for m in matches], depth_result)
         # TODO: scale coordinates from scene resolution to render resolution?
         sce_kp_2d = np.array([sce_kp[m.queryIdx].pt for m in matches], dtype='float')
-        
-#        print('3d z-range: %s'%(ref_kp_3d.ptp(axis=(0,2)),), flush=True)
+
+        if DEBUG:
+            print('3d z-range: %s'%(ref_kp_3d.ptp(axis=0),), flush=True)
         
         # solve pnp with ransac
         rvec, tvec, inliers = self._solve_pnp_ransac(sce_kp_2d, ref_kp_3d)
@@ -72,10 +76,12 @@ class KeypointAlgo():
             print('inliers: %s/%s'%(len(inliers), len(matches)), flush=True)
         self._draw_matches(sce_img, sce_kp, ref_img, ref_kp,
                            [matches[i[0]] for i in inliers], label='inliers', pause=False)
-                               
         
         # set model params to solved pose & pos
         self._set_sc_from_ast_rot_and_trans(rvec, tvec)
+        
+        if BATCH_MODE and self.debug_filebase:
+            self.glWidget.saveViewToFile(self.debug_filebase+'r.png')
     
     
     def _orb_features(self, scene_img, model_img):
@@ -151,7 +157,7 @@ class KeypointAlgo():
         return matches
 
     
-    def _draw_matches(self, img1, kp1, img2, kp2, matches, pause=True, label='matches'):
+    def _draw_matches(self, img1, kp1, img2, kp2, matches, pause=True, show=True, label='matches'):
         matches = list([m] for m in matches)
         draw_params = {
 #            matchColor: (88, 88, 88),
@@ -176,7 +182,11 @@ class KeypointAlgo():
         for kp in kp1:
             kp.pt = (kp.pt[0]*CAMERA_WIDTH/VIEW_WIDTH, kp.pt[1]*CAMERA_HEIGHT/VIEW_HEIGHT)
 
-        cv2.imshow(label, img3)
+        if BATCH_MODE and self.debug_filebase:
+            cv2.imwrite(self.debug_filebase+label[:1]+'.png', img3)
+        
+        if show:
+            cv2.imshow(label, img3)
         if pause:
             cv2.waitKey()
     
@@ -184,13 +194,13 @@ class KeypointAlgo():
     def _solve_pnp_ransac(self, sce_kp_2d, ref_kp_3d):
         
         # assuming no lens distortion
-        dist_coeffs = np.zeros((4,1), dtype="float")
+        dist_coeffs = None
         ref_kp_3d = np.reshape(ref_kp_3d, (len(ref_kp_3d),1,3))
         sce_kp_2d = np.reshape(sce_kp_2d, (len(sce_kp_2d),1,2))
         
         try:
             retval, rvec, tvec, inliers = cv2.solvePnPRansac(
-                    ref_kp_3d, sce_kp_2d, intrinsic_camera_mx(), dist_coeffs,
+                    ref_kp_3d, sce_kp_2d, tools.intrinsic_camera_mx(), dist_coeffs,
                     iterationsCount = self.RANSAC_ITERATIONS,
                     reprojectionError = self.RANSAC_ERROR)
             if not retval:
@@ -198,6 +208,8 @@ class KeypointAlgo():
             if len(inliers) < self.MIN_FEATURES:
                 raise PositioningException('RANSAC algorithm was left with too few inliers')
         except Exception as e:
+            if DEBUG:
+                print(str(e))
             raise PositioningException('RANSAC algorithm ran into problems') from e
 
         return rvec, tvec, inliers
@@ -206,16 +218,29 @@ class KeypointAlgo():
     def _inverse_project(self, points_2d, depths):
         z0 = self.system_model.z_off.value # translate to object origin
         def invproj(xi, yi):
-            d = depths[int(yi)][int(xi)]
-            x, y = tools.calc_xy(xi, yi, -d, width=VIEW_WIDTH, height=VIEW_HEIGHT)
-            return x, y, -d-z0
+            z = -depths[int(yi)][int(xi)]
+            x, y = tools.calc_xy(xi, yi, z, width=VIEW_WIDTH, height=VIEW_HEIGHT)
+            return x, -y, -(z-z0) # same as rotate using cv2gl_q
         
         points_3d = np.array([invproj(pt[0], pt[1]) for pt in points_2d])
         return points_3d
     
     
     def _set_sc_from_ast_rot_and_trans(self, rvec, tvec):
-        self.system_model.set_spacecraft_pos([tvec[0], -tvec[1], -tvec[2]])
-        self.system_model.rotate_spacecraft(
-                quaternion.from_rotation_vector(rvec).conj())
+        # from opencv cam frame (axis: +z, up: -y) to opengl (axis -z, up: +y)
+        # by rotating 180deg around x-axis
+        cv2gl_q = np.quaternion(0, 1, 0, 0)
+        self.system_model.spacecraft_pos = tools.q_times_v(cv2gl_q, tvec)#[tvec[0], -tvec[1], -tvec[2]]
+        
+        # from opencv cam frame to spacecraft cam frame
+        cv2sc_q = cv2gl_q * self.system_model.sc2gl_q.conj()
+        
+        cv_cam_delta_q = tools.angleaxis_to_q(rvec)
+        sc_delta_q =  cv2sc_q.conj() * cv_cam_delta_q.conj() * cv2sc_q
+        
+        self.system_model.rotate_spacecraft(sc_delta_q)
+
+        # for some reason rotating asteroid instead of spacecraft 
+        # doesnt work as simply as below
+        #self.system_model.rotate_asteroid(sc_delta_q.conj())
         

src/algo/model.py
@@ -5,6 +5,7 @@ import numpy as np
 import quaternion # adds to numpy
 from astropy.time import Time
 from astropy import constants as const
+import configparser
 
 from settings import *
 from algo import tools
@@ -17,6 +18,7 @@ class Parameter():
         self._def_val = def_val
         self._value = self.def_val
         self.is_gl_z = is_gl_z
+        self.real_value = None
         self.change_callback = None
         self.fire_change_events = True
     
@@ -96,12 +98,10 @@ class Parameter():
 
 class SystemModel():
     # from sc cam frame (axis: +x, up: +z) to opengl (axis -z, up: +y)
-    q_sc2gl = np.quaternion(0.5, 0.5, -0.5, -0.5)
+    sc2gl_q = np.quaternion(0.5, 0.5, -0.5, -0.5)
     
     def __init__(self, *args, **kwargs):
         self.asteroid = Asteroid()
-        
-        self.real_sc_pos = None
         
         # spacecraft position relative to asteroid, z towards spacecraft,
         #   x towards right when looking out from s/c camera, y up
@@ -121,10 +121,7 @@ class SystemModel():
         self.ast_x_rot = Parameter(-90, 90, estimate=False) # axis latitude
         self.ast_y_rot = Parameter(-180, 180, estimate=False) # axis longitude
         self.ast_z_rot = Parameter(-180, 180, estimate=False) # rotation
-        self.ast_x_rot.value = math.degrees(self.asteroid.axis_latitude)
-        self.ast_y_rot.value = math.degrees(self.asteroid.axis_longitude)
-        self.ast_z_rot.value = \
-                (math.degrees(self.asteroid.rotation_pm) + 180) % 360 - 180
+        self.asteroid_rotation_from_model()
 
         # time in seconds since 1970-01-01 00:00:00
         self.time = Parameter(
@@ -157,11 +154,8 @@ class SystemModel():
     def spacecraft_pos(self):
         return self.x_off.value, self.y_off.value, self.z_off.value
 
-    @property
-    def spacecraft_dist(self):
-        return math.sqrt(sum(x**2 for x in self.spacecraft_pos))
-
-    def set_spacecraft_pos(self, pos):
+    @spacecraft_pos.setter
+    def spacecraft_pos(self, pos):
         self.z_off.value = pos[2]
 
         half_range = abs(pos[2] / 170 * 4)
@@ -174,44 +168,115 @@ class SystemModel():
     @property
     def spacecraft_rot(self):
         return self.x_rot.value, self.y_rot.value, self.z_rot.value
-    
-    def set_spacecraft_rot(self, r):
-        self.x_rot.value, self.y_rot.value, self.z_rot.value = r
-    
-    def rotate_spacecraft(self, q):
-        new_q = self.spacecraft_q() * q
-        self.x_rot.value, self.y_rot.value, self.z_rot.value = \
-            list(map(math.degrees, tools.q_to_spherical(new_q)))
 
-    def spacecraft_q(self):
-        return tools.spherical_to_q(*list(map(
-                math.radians,
-                (self.x_rot.value, self.y_rot.value, self.z_rot.value)
-        )))
+    @spacecraft_rot.setter
+    def spacecraft_rot(self, r):
+        self.x_rot.value, self.y_rot.value, self.z_rot.value = r
+
+    @property
+    def asteroid_axis(self):
+        return self.ast_x_rot.value, self.ast_y_rot.value, self.ast_z_rot.value
     
-    def sc_asteroid_rel_rot(self):
-        """ rotation of asteroid relative to spacecraft in opengl coords """
-        
+    @asteroid_axis.setter
+    def asteroid_axis(self, r):
+        self.ast_x_rot.value, self.ast_y_rot.value, self.ast_z_rot.value = r
+
+    @property
+    def spacecraft_dist(self):
+        return math.sqrt(sum(x**2 for x in self.spacecraft_pos))
+
+    def asteroid_rotation_from_model(self):
+        self.ast_x_rot.value = math.degrees(self.asteroid.axis_latitude)
+        self.ast_y_rot.value = math.degrees(self.asteroid.axis_longitude)
+        self.ast_z_rot.value = (math.degrees(self.asteroid.rotation_pm) + 180) % 360 - 180
+
+    def update_asteroid_model(self):
         self.asteroid.axis_latitude = math.radians(self.ast_x_rot.value)
         self.asteroid.axis_longitude = math.radians(self.ast_y_rot.value)
         self.asteroid.rotation_pm = math.radians(self.ast_z_rot.value)
+
+    @property
+    def real_spacecraft_pos(self):
+        return self.x_off.real_value, self.y_off.real_value, self.z_off.real_value
+    
+    @real_spacecraft_pos.setter
+    def real_spacecraft_pos(self, rv):
+        self.x_off.real_value, self.y_off.real_value, self.z_off.real_value = rv
+
+    @property
+    def real_spacecraft_rot(self):
+        return self.x_rot.real_value, self.y_rot.real_value, self.z_rot.real_value
+    
+    @real_spacecraft_rot.setter
+    def real_spacecraft_rot(self, rv):
+        self.x_rot.real_value, self.y_rot.real_value, self.z_rot.real_value = rv
+
+    @property
+    def real_asteroid_axis(self):
+        return self.ast_x_rot.real_value, self.ast_y_rot.real_value, self.ast_z_rot.real_value
+    
+    @real_asteroid_axis.setter
+    def real_asteroid_axis(self, rv):
+        self.ast_x_rot.real_value, self.ast_y_rot.real_value, self.ast_z_rot.real_value = rv
+
+
+    def rotate_spacecraft(self, q):
+        new_q = self.spacecraft_q() * q
+        self.x_rot.value, self.y_rot.value, self.z_rot.value = \
+            list(map(math.degrees, tools.q_to_ypr(new_q)))
+
+    def rotate_asteroid(self, q):
+        new_q = self.asteroid.rotation_q(self.time.value) * q
+        self.x_rot.value, self.y_rot.value, self.z_rot.value = \
+            list(map(math.degrees, tools.q_to_ypr(new_q)))
+
+    def spacecraft_q(self):
+        return tools.ypr_to_q(*list(map(
+                math.radians,
+                (self.x_rot.value, self.y_rot.value, self.z_rot.value)
+        )))
         
-        ast_q = self.asteroid.rotation_q(self.time.value)
-        sc_q = self.spacecraft_q()
+    def asteroid_q(self):
+        return self.asteroid.rotation_q(self.time.value)
+    
+    def gl_sc_asteroid_rel_rot(self):
+        """ rotation of asteroid relative to spacecraft in opengl coords """
+        self.update_asteroid_model()
         
         # ast => eq => sc => opengl
-        q_tot = ast_q.conj() * sc_q * SystemModel.q_sc2gl
+        q_tot = SystemModel.sc2gl_q.conj() * self.sc_asteroid_rel_rot()
         
         if not BATCH_MODE and DEBUG:
-            print('asteroid x-axis: %s'%tools.q_times_v(q_tot.conj(), np.array([1, 0, 0])))
+            print('asteroid x-axis: %s'%tools.q_times_v(q_tot, np.array([1, 0, 0])))
         
-        return q_tot.conj()
+        return q_tot
+    
+    
+    def sc_asteroid_rel_rot(self, time=None):
+        ast_q = self.asteroid.rotation_q(time or self.time.value)
+        sc_q = self.spacecraft_q()
+        return sc_q.conj() * ast_q
+
+    def real_sc_asteroid_rel_rot(self):
+        org_sc_rot = self.spacecraft_rot
+        org_ast_axis = self.asteroid_axis
+        self.spacecraft_rot = self.real_spacecraft_rot
+        self.asteroid_axis = self.real_asteroid_axis
+        self.update_asteroid_model()
+        
+        q_tot = self.sc_asteroid_rel_rot(time=self.time.real_value)
+        
+        self.spacecraft_rot = org_sc_rot
+        self.asteroid_axis = org_ast_axis
+        self.update_asteroid_model()
+        return q_tot
+    
     
     def light_rel_dir(self):
         """ direction of light relative to spacecraft in opengl coords """
         ast_v = tools.normalize_v(self.asteroid.position(self.time.value))
         sc_q = self.spacecraft_q()
-        return tools.q_times_v(SystemModel.q_sc2gl.conj() * sc_q.conj(), ast_v)
+        return tools.q_times_v(SystemModel.sc2gl_q.conj() * sc_q.conj(), ast_v)
         
     def solar_elongation(self):
         ast_v = self.asteroid.position(self.time.value)
@@ -221,6 +286,34 @@ class SystemModel():
             print('elong: %.3f | dir: %.3f' % (
                 math.degrees(elong), math.degrees(direc)))
         return elong, direc
+    
+    def save_state(self, filename):
+        config = configparser.ConfigParser()
+        filename = filename+('.lbl' if len(filename)<5 or filename[-4:]!='.lbl' else '')
+        config.read(filename)
+        config.add_section('main')
+        config.add_section('real')
+        
+        for n, p in self.get_params(all=True):
+            config.set('main', n, str(p.value))
+            if p.real_value is not None:
+                config.set('real', n, str(p.real_value))
+
+        with open(filename, 'w') as f:
+            config.write(f)
+    
+    def load_state(self, filename):
+        config = configparser.ConfigParser()
+        filename = filename+('.lbl' if len(filename)<5 or filename[-4:]!='.lbl' else '')
+        config.read(filename)
+        
+        for n, p in self.get_params(all=True):
+            p.value = float(config.get('main').get(n))
+            if config.get('real').get(n, None) is not None:
+                p.real_value = float(config.get('real').get(n, None))
+        
+        self.update_asteroid_model()
+        assert np.isclose(self.time.value, time), 'Failed to set time value'
     
     def __repr__(self):
         return (
@@ -297,7 +390,7 @@ class Asteroid():
         # TODO: use precession info
         q_x2z = np.quaternion(1, 0, 1, 0).normalized()
         return q_x2z \
-                * tools.spherical_to_q(self.axis_latitude, self.axis_longitude, theta) \
+                * tools.ypr_to_q(self.axis_latitude, self.axis_longitude, theta) \
                 * q_x2z.conj()
     
     def position(self, timestamp):

src/algo/tools.py
@@ -76,12 +76,12 @@ def angle_between_v(v1, v2):
 def angle_between_q(q1, q2):
     # from  https://chrischoy.github.io/research/measuring-rotation/
     qd = q1.conj()*q2
-    return 2*math.atan(qd.w/np.linalg.norm(qd.vec))
+    return 2*math.acos(qd.normalized().w)
 
 
 def angle_between_ypr(ypr1, ypr2):
-    q1 = spherical_to_q(*ypr1)
-    q2 = spherical_to_q(*ypr2)
+    q1 = ypr_to_q(*ypr1)
+    q2 = ypr_to_q(*ypr2)
     return angle_between_q(q1, q2)
 
 
@@ -98,22 +98,27 @@ def equatorial_to_ecliptic(ra, dec):
     return math.radians(sc.lat.value), math.radians(sc.lon.value)
     
     
-def q_to_angleaxis(q):
+def q_to_angleaxis(q, compact=False):
     theta = math.acos(q.w) * 2.0
-    return theta, normalize_v(np.array([q.x, q.y, q.z]))
+    return (theta,) + tuple(normalize_v(np.array([q.x, q.y, q.z])))
 
 
-def angleaxis_to_q(theta, v):
+def angleaxis_to_q(rv):
+    if len(rv)==4:
+        theta = rv[0]
+        v = normalize_v(np.array(rv[1:]))
+    elif len(rv)==3:
+        theta = math.sqrt(sum(x**2 for x in rv))
+        v = np.array(rv)/theta
+    else:
+        raise Exception('Invalid angle-axis vector: %s'%(rv,))
+    
     w = math.cos(theta/2)
-    try:
-        sc = 1/math.sqrt(sum(map(lambda x: x**2, v)))
-        v = list(map(lambda x: x*math.sin(theta/2)*sc, v))
-    except Exception as e:
-        raise Exception('%s'%v) from e
+    v = v*math.sin(theta/2)
     return np.quaternion(w, *v).normalized()
 
 
-def spherical_to_q(lat, lon, roll):
+def ypr_to_q(lat, lon, roll):
     # Tait-Bryan angles, aka yaw-pitch-roll, nautical angles, cardan angles
     # intrinsic euler rotations z-y'-x'', pitch=-lat, yaw=lon
     return (
@@ -123,7 +128,7 @@ def spherical_to_q(lat, lon, roll):
     )
     
     
-def q_to_spherical(q):
+def q_to_ypr(q):
     # from https://math.stackexchange.com/questions/687964/getting-euler-tait-bryan-angles-from-quaternion-representation
     q0,q1,q2,q3 = quaternion.as_float_array(q)[0]
     roll = np.arctan2(q2*q3+q0*q1, .5-q1**2-q2**2)

src/lbl_loader.py
@@ -123,7 +123,7 @@ def load_image_meta(src, sm):
     sc_ast_ec_p = np.array([sc.x.value, sc.y.value, sc.z.value])
 
     # s/c orientation
-    scoq = tools.spherical_to_q(*sco)
+    scoq = tools.ypr_to_q(*sco)
 
     # project old position to new base vectors
     scub = tools.q_to_unitbase(scoq)
@@ -145,9 +145,9 @@ def load_image_meta(src, sm):
             ast_sc_p,
         ))
 
-    sm.real_sc_pos = ast_sc_p
+    sm.real_spacecraft_pos = ast_sc_p
     if USE_IMG_LABEL_FOR_SC_POS:
-        sm.set_spacecraft_pos(ast_sc_p)
+        sm.spacecraft_pos = ast_sc_p
     ##
     ## done setting spacecraft position
 

src/testloop.py
@@ -16,7 +16,8 @@ import numpy as np
 import quaternion
 from astropy.coordinates import spherical_to_cartesian
 
-from algo.tools import (spherical_to_q, q_times_v, q_to_unitbase, normalize_v,
+import algo.tools as tools
+from algo.tools import (ypr_to_q, q_to_ypr, q_times_v, q_to_unitbase, normalize_v,
                    wrap_rads, solar_elongation, angle_between_ypr)
 from algo.tools import PositioningException
 from algo.centroid import CentroidAlgo
@@ -70,21 +71,25 @@ class TestLoop():
                 'iter', 'date', 'execution time',
                 'time', 'ast lat', 'ast lon', 'ast rot',
                 'sc lat', 'sc lon', 'sc rot', 
+                'rel yaw', 'rel pitch', 'rel roll', 
                 'sol elong', 'light dir', 'x sc pos', 'y sc pos', 'z sc pos',
                 'imgfile', 'optfun val',
                 'time dev', 'ast lat dev', 'ast lon dev', 'ast rot dev',
                 'sc lat dev', 'sc lon dev', 'sc rot dev', 'total dev angle',
                 'x est sc pos', 'y est sc pos', 'z est sc pos',
-                'x err sc pos', 'y err sc pos', 'z err sc pos',
+                'yaw rel est', 'pitch rel est', 'roll rel est',
+                'x err sc pos', 'y err sc pos', 'z err sc pos', 'rot error',
                 'lat error', 'dist error',
             ))+'\n')
 
-        ex_times, laterrs, disterrs, fails, li = [], [], [], 0, 0
+        ex_times, laterrs, disterrs, roterrs, fails, li = [], [], [], [], 0, 0
         for i in range(times):
             self._maybe_exit()
             
-            etime, params, noise, pos, fvals, err = \
+            etime, params, noise, pos, rel_rot, fvals, err = \
                     self._mainloop(imgdir, **kwargs)
+
+#            print('err: %s'%(err,))
 
             # print out progress
             if math.floor(100*i/times) > li:
@@ -92,15 +97,16 @@ class TestLoop():
                 li += 1
 
             # calculate distance error
-            dist = abs(params[-3])
+            dist = abs(params[-6])
             if not math.isnan(err[0]):
                 lerr = math.sqrt(err[0]**2 + err[1]**2) / dist
                 derr = abs(err[2]) / dist
+                rerr = abs(err[3])
                 laterrs.append(lerr)
                 disterrs.append(derr)
+                roterrs.append(rerr)
             else:
-                lerr = float('nan')
-                derr = lerr
+                lerr = derr = rerr = float('nan')
                 fails += 1
 
             ex_times.append(etime)
@@ -109,31 +115,33 @@ class TestLoop():
             with open(logfile, 'a') as file:
                 file.write('\t'.join(map(str, (
                     i, dt.now().strftime("%Y-%m-%d %H:%M:%S"), etime,
-                    *params, *noise, *pos, *err, lerr, derr
+                    *params, *noise, *pos, *rel_rot, *err, lerr, derr
                 )))+'\n')
                 
             # log opt fun values in other file
             with open(fval_logfile, 'a') as file:
                 file.write('\t'.join(map(str, fvals or []))+'\n')
         
+        prctls = (50, 68, 95) + ((99.7,) if times>=2000 else tuple())
         calc_prctls = lambda errs: \
                 ', '.join('%.2f' % p
-                for p in 100*np.nanpercentile(errs, (50, 68, 95, 99.7)))
+                for p in 100*np.nanpercentile(errs, prctls))
         try:
             laterr_pctls = calc_prctls(laterrs)
             disterr_pctls = calc_prctls(disterrs)
+            roterr_pctls = ', '.join(['%.2f'%p for p in np.nanpercentile(roterrs, prctls)])
         except Exception as e:
             print('Error calculating quantiles: %s'%e)
             laterr_pctls = 'error'
             disterr_pctls = 'error'
+            roterr_pctls = 'error'
         
         # a summary line
         summary = (
-            '%s - time: %.1fh (%.0fs), '
-            + 'lat-err avg: %.2f%%, '
-            + 'lat-err qs%%: (%s), '
-            + 'dist-err avg: %.2f%%, '
-            + 'dist-err qs%%: (%s), '
+            '%s - t: %.1fh (%.0fs), '
+            + 'Le: %.2f%% (%s), '
+            + 'De: %.2f%% (%s), '
+            + 'Re: %.2f° (%s), '
             + 'fail: %.1f%% \n'
         ) % (
             dt.now().strftime("%Y-%m-%d %H:%M:%S"),
@@ -143,6 +151,8 @@ class TestLoop():
             laterr_pctls,
             100*sum(disterrs)/len(disterrs),
             disterr_pctls,
+            sum(roterrs)/len(roterrs),
+            roterr_pctls,
             100*fails/times,
         )
         
@@ -185,19 +195,19 @@ class TestLoop():
             sco_lat = wrap_rads(-sc_lat + da*math.sin(dd))
             sco_lon = wrap_rads(math.pi + sc_lon + da*math.cos(dd))
             sco_rot = np.random.uniform(-math.pi, math.pi) # rotation around camera axis
-            sco_q = spherical_to_q(sco_lat, sco_lon, sco_rot)
+            sco_q = ypr_to_q(sco_lat, sco_lon, sco_rot)
             
             # sc_ast_p ecliptic => sc_ast_p open gl -z aligned view
-            sc_x, sc_y, sc_z = q_times_v((sco_q * sm.q_sc2gl).conj(), sc_ast_v)
+            sc_x, sc_y, sc_z = q_times_v((sco_q * sm.sc2gl_q).conj(), sc_ast_v)
             
             # asteroid rotation axis, add zero mean gaussian with small variance
             da = np.random.uniform(0, rad(self._noise_ast_rot_axis))
             dd = np.random.uniform(0, 2*math.pi)
             ax_lat_true = sm.asteroid.axis_latitude
             ax_lon_true = sm.asteroid.axis_longitude
-            ax_lat = ax_lat_true + da*math.sin(dd)
-            ax_lon = ax_lon_true + da*math.cos(dd)
-            ast_q = sm.asteroid.rotation_q(time)
+            ast_q_true = sm.asteroid.rotation_q(time)
+            est_ax_lat = ax_lat_true + da*math.sin(dd)
+            est_ax_lon = ax_lon_true + da*math.cos(dd)
             
             # get asteroid position so that know where sun is
             # *actually barycenter, not sun
@@ -221,12 +231,11 @@ class TestLoop():
         meas_sco_rot = wrap_rads(sco_rot 
                 + np.random.normal(0, rad(self._noise_sco_rot)))
 
-
         ## based on above, call VISIT to make a target image
         ##
-        light = normalize_v(q_times_v(ast_q.conj(), np.array([as_x, as_y, as_z])))
-        focus = q_times_v(ast_q.conj(), np.array([sc_ex, sc_ey, sc_ez]))
-        view_x, ty, view_z = q_to_unitbase(ast_q.conj() * sco_q)
+        light = normalize_v(q_times_v(ast_q_true.conj(), np.array([as_x, as_y, as_z])))
+        focus = q_times_v(ast_q_true.conj(), np.array([sc_ex, sc_ey, sc_ez]))
+        view_x, ty, view_z = q_to_unitbase(ast_q_true.conj() * sco_q)
 
         # in VISIT focus & view_normal are vectors pointing out from the object,
         # light however points into the object
@@ -254,7 +263,7 @@ class TestLoop():
         if False:
             tx, ty, tz = q_times_v((sco_q).conj(), sc_ast_v)
             print('%s'%''.join('%s: %s\n'%(k,v) for k,v in (
-                ('ast_x_v', list(q_times_v(ast_q, np.array([1, 0, 0])))),
+                ('ast_x_v', list(q_times_v(ast_q_true, np.array([1, 0, 0])))),
                 ('sc_lat', sc_lat),
                 ('sc_lon', sc_lon),
                 ('sco_lat', sco_lat),
@@ -265,18 +274,33 @@ class TestLoop():
                 ('gl_sc_ast_v', [sc_x, sc_y, sc_z]),
             )))
 
-        # set datetime, spacecraft & asteroid orientation
-        sm.time.value = meas_time
-        sm.x_rot.value = deg(meas_sco_lat)
-        sm.y_rot.value = deg(meas_sco_lon)
-        sm.z_rot.value = deg(meas_sco_rot)
-        sm.asteroid.axis_latitude = ax_lat
-        sm.asteroid.axis_longitude = ax_lon
-        sm.real_sc_pos = [sc_x, sc_y, sc_z]
+        # save real values used for comparison
+        sm.time.real_value = time
+        sm.real_spacecraft_pos = sc_x, sc_y, sc_z
+        sm.real_spacecraft_rot = deg(sco_lat), deg(sco_lon), deg(sco_rot)
+        sm.asteroid.axis_latitude = ax_lat_true
+        sm.asteroid.axis_longitude = ax_lon_true
+        #sm.asteroid.rotation_pm is what it is (time counts for the random element)
+        sm.asteroid_rotation_from_model()
+        for n in ('x', 'y', 'z'):
+            p = getattr(sm, 'ast_'+n+'_rot')
+            p.real_value = p.value
+        
+        # set datetime, spacecraft & asteroid orientation to sample values
+        sm.time.value = meas_time; assert np.isclose(sm.time.value, meas_time), 'Failed to set time value'
+        sm.spacecraft_pos = (0, 0, MIN_DISTANCE*2) # set to default value
+        sm.spacecraft_rot = (deg(meas_sco_lat), deg(meas_sco_lon), deg(meas_sco_rot))
+        sm.asteroid.axis_latitude = est_ax_lat
+        sm.asteroid.axis_longitude = est_ax_lon
+        #sm.asteroid.rotation_pm is what it is
+        sm.asteroid_rotation_from_model() # set ast rotation params
+
+        # save state to file
+        sm.save_state(imgfile[0:-4])
 
         # load image & run optimization algo(s)
         ok = self._run_algo(imgfile, **kwargs)
-        
+
         # save function values from optimization
         fvals = self.window.phasecorr.optfun_values \
                 if ok and kwargs.get('method', False)=='phasecorr' \
@@ -293,28 +317,38 @@ class TestLoop():
             sm.z_rot.value = deg(sco_rot)
             sm.asteroid.axis_latitude = ax_lat_true
             sm.asteroid.axis_longitude = ax_lon_true
-            sm.set_spacecraft_pos((sc_x, sc_y, sc_z))
+            sm.spacecraft_pos = (sc_x, sc_y, sc_z)
 
         # assemble return values
         etime = (dt.now() - start_time).total_seconds()
+        real_rel_rot = q_to_ypr(sm.real_sc_asteroid_rel_rot())
         params = (time, deg(ax_lat_true), deg(ax_lon_true),
                 deg(sm.asteroid.rotation_theta(time)),
                 deg(sco_lat), deg(sco_lon), deg(sco_rot),
-                deg(elong), deg(direc), sc_x, sc_y, sc_z, imgfile, final_fval)
-        
+                deg(elong), deg(direc), sc_x, sc_y, sc_z,
+                deg(real_rel_rot[0]), deg(real_rel_rot[1]), deg(real_rel_rot[2]),
+                imgfile, final_fval)
         time_noise = meas_time - time
         ast_rot_noise = 2*math.pi * time_noise/sm.asteroid.rotation_period
-        noise = (ax_lat-ax_lat_true, ax_lon-ax_lon_true, ast_rot_noise,
+        noise = (est_ax_lat-ax_lat_true, est_ax_lon-ax_lon_true, ast_rot_noise,
                 meas_sco_lat-sco_lat, meas_sco_lon-sco_lon, meas_sco_rot-sco_rot)
         dev_angle = angle_between_ypr(noise[0:3], noise[3:6])
         noise = (time_noise,) + tuple(map(deg, noise + (dev_angle,)))
         
-        pos = (sm.x_off.value, sm.y_off.value, sm.z_off.value)
-        err = (sm.x_off.value-sc_x, sm.y_off.value-sc_y, sm.z_off.value-sc_z)
         if not ok:
-            err = pos = float('nan')*np.ones(3)
+            pos = float('nan')*np.ones(3)
+            rel_rot = float('nan')*np.ones(3)
+            err = float('nan')*np.ones(4)
+            
+        else:
+            pos = sm.spacecraft_pos
+            rel_rot = q_to_ypr(sm.sc_asteroid_rel_rot())
+            err = (
+                *np.subtract(pos, sm.real_spacecraft_pos),
+                deg(angle_between_ypr(rel_rot, real_rel_rot)),
+            )
 
-        return etime, params, noise, pos, fvals, err
+        return etime, params, noise, pos, map(deg, rel_rot), fvals, err
     
     
     def _run_algo(self, imgfile_, **kwargs):